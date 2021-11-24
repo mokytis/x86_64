629d42a88254ea653fbabf7c5930c3630c6e7e71
Luke Spademan <info@lukespademan.com>
2021-11-24 16:34:42 +0000

    fib outputValue uses local variables

--- src/fib.asm
+++ src/fib.asm
@@ -73,67 +73,50 @@ _fininput:
 	ret
 
 _outputValue:
-  ; write a base 10 ascii representation of rbx to stdout
+	; output ascii for a number (in rbx) and a newline
+
+	; create stackframe
+	push rbp
+	mov rbp, rsp
+	sub rsp, 16  ; we have 16 bytes of local variables
 
 	; we start by adding the values to the stack
 	; becuause we need to revese endieness
-  mov dword [digits], 0  ; digits counts how many digits we are outputting
+	push 10
+	mov dword [rbp  - 8], 2  ; digits (bytes to output)
+	mov dword [rbp - 16], 0  ; digit
+
 
 _output_addvaluestostack:
-	; loop that uses divmod
+	; does a divmod 10
+	; adds 48 (ascii int offset) to remainder and pushes it to the stack
+	; repeats with the division result until the result is zero
 
-	; rdx will store remainder
-	; rcx is the divisor
-	; rax is the value to divide and will be the result of the division
 	mov rdx, 0
 	mov rcx, 10
 	mov rax, rbx
 	div rcx
 
-	add rdx, 48  ; 48 is the ascii off set of digits
-	push dx  ; add the ascii for digit to the stack
-	add dword [digits], 1 ; inc out digit counter
+	add rdx, 48 ; ascii int offset (ascii 48 = 0 ascii 49 = 1 etc.)
+	push dx
+	add dword [rbp  - 8], 2  ; add 2 to digits (each digit is 2 bytes)
 
 	mov rbx, rax
-
-	; if we have processed the whole value jump to the output section
-	; if not carry on adding values ot the stack
 	cmp rax, 0
 	je _output_writevaluesfromstack
 	jmp _output_addvaluestostack
 
 _output_writevaluesfromstack:
-	; loop exit condition
-	; exit loop if counter is zero
-	cmp dword [digits], 0
-	je _output_newlineandreturn
+  mov rax, 1
+	mov rdi, 1
+	mov rsi, rsp
+	mov rdx, [rbp - 8] ; length
+	syscall
 
-	pop ax  ; get digit to output off the stack
-	mov [digit], ax  ; and write it to a buffer
-
-	; output the digit
-  mov rax, 1  ; write sys call
-	mov rdi, 1  ; write to stdout
-	mov rsi, digit  ; memory location of current digit to output
-	mov rdx, 1  ; length of buffer is 1
-	syscall  ; make the write syscall
-
-	; decrease digit counter
-	sub dword [digits], 1
-	jmp _output_writevaluesfromstack
-
-_output_newlineandreturn:
-  ; write a newline to stdout and return from function
-  mov rax, 1      ; write syscall
-	mov rdi, 1      ; fd for stdout
-	mov rsi, newline  ; buffer location for \n
-	mov rdx, 1  ; buffer length is just 1
-	syscall  ; make the syscall
-	ret  ; return from function. we have finished outputting
+	leave
+	ret
 
 section .bss
-	digits: resb 8
-	digit: resb 8
 	input: resb 8
 	index: resb 8
 	counter: resb 8

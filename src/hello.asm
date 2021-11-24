29a24f2127e7bbbee3b1f3ea1b1a41e6623ba049
Luke Spademan <info@lukespademan.com>
2021-11-24 14:46:45 +0000

    commented syscalls in hello.asm

--- src/hello.asm
+++ src/hello.asm
@@ -1,16 +1,17 @@
 section .data
+	; 10 is the decimal of ascii new line (\n)
 	text db "Hello, World!", 10
 
 section .text
 	global _start
 
 _start:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, text
-	mov rdx, 14
+	mov rax, 1    ; code for write syscall
+	mov rdi, 1    ; write to stdout stdout
+	mov rsi, text ; memory location of buffer
+	mov rdx, 14   ; length of buffer
 	syscall
 
-	mov rax, 60
-	mov rdi, 0
+	mov rax, 60 ; exit syscall
+	mov rdi, 0  ; exit code
 	syscall

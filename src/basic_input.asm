c3852c210b81586207152fedf823aee53cee3a5a
Luke Spademan <info@lukespademan.com>
2021-11-24 15:31:08 +0000

    doucmented syscalls in basicinput

--- src/basic_input.asm
+++ src/basic_input.asm
@@ -14,23 +14,23 @@ _start:
 	call _getName
 	call _outputName
 
-	mov rax, 60
-	mov rdi, 0
+	mov rax, 60  ; 60 = exit syscall
+	mov rdi, 0   ; 0 = standard exit status code
 	syscall
 
 _askName:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, text1
-	mov rdx, 19
+	mov rax, 1  ; 60 = write syscall
+	mov rdi, 1  ; 1 = file descriptor (fd 1 = stdout)
+	mov rsi, text1  ; text1 = buffer to output
+	mov rdx, 19  ; 19 = length of buffer
 	syscall
 	ret
 
 _getName:
-	mov rax, 0
-	mov rdi, 0
-	mov rsi, name
-	mov rdx, 16
+	mov rax, 0  ; 0 = read syscall
+	mov rdi, 0  ; 0 = file descriptor (fd 0 = stdin)
+	mov rsi, name  ; buffer to write to
+	mov rdx, 16 ; num of bytes to read
 	syscall
 	ret
 
